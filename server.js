889b962f399e72174d341d7aa381bacf162cfccc
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-20 16:41:38 -0500

    Completed post route

--- server.js
+++ server.js
@@ -33,4 +33,32 @@ server.get('/recipes/details/:name', (req, res) => {
   }
 });
 
+// POST route for adding new recipes
+server.post('/recipes', (req, res) => {
+  const { name, ingredients, instructions } = req.body;
+
+  // Verifying that all required data is included
+  if (!name || !ingredients || !instructions) {
+    res.status(400).json({
+      "error": "Recipe must include a name, ingredients, and instructions!"
+    });
+  } else {
+    let recipeIndex = data.recipes.findIndex(x => x.name == name);
+    
+    if (recipeIndex != -1) {
+      res.status(400).json({
+        "error": "Recipe already exists"
+      });
+    } else {
+      data.recipes.push(req.body);
+
+      res.status(201).json();
+    };
+  };
+
+
+});
+
+
+
 module.exports = server;
