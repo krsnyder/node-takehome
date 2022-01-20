e9f087b5c33eadc93ddf7418387fefad53d15968
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-19 20:01:21 -0500

    quick refactor

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const app = express();
+const server = require('./server');
 const PORT = '3000';
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
