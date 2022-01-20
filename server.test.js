9afa468af2df79177e83a778105985c4aeb3950f
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-19 21:15:45 -0500

    First successful server test

--- server.test.js
+++ server.test.js
@@ -1,6 +1,17 @@
+const request = require('supertest');
 const server = require('./server');
 
 it('sanity check', () => {
   expect(true).not.toBe(false);
 });
 
+describe('GET /recipes', () => {
+  test("Should return list of recipe names", async () => {
+    const response = await request(server).get("/recipes");
+    expect(response.statusCode).toBe(200);
+  })
+  // Should respond with a 200 status code
+  // should specify json in the content header type
+
+
+})
