adfe93bd4331d462c1fdbe25a8c18f719a42a7f7
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-19 21:28:44 -0500

    testing content header type of get response

--- server.test.js
+++ server.test.js
@@ -10,15 +10,17 @@ describe('GET /recipes', () => {
   test("Should respond with a 200 status code", async () => {
     const response = await request(server).get("/recipes");
     expect(response.statusCode).toBe(200);
-  })
-  // 
+  });
+  
   test("Should return list of recipe names", async () => {
     const response = await request(server).get("/recipes");
     const { recipeNames } = response.body;
     
     expect(recipeNames).toBeInstanceOf(Array);
-  })
-  // should specify json in the content header type
+  });
+  test("should specify json in the content header type", async () => {
+    const response = await request(server).get("/recipes");
 
-
-})
+    expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
+  });
+});
