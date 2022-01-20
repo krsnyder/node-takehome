cbf9a74545b0dfb9e06aa01176147380ae374b3a
Kirk Snyder Jr <Kirk.Robert.Snyder@gmail.com>
2022-01-19 20:12:00 -0500

    added test data, completed initial GET endpoint

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const server = express();
+const data = require('./data.json')
+
+server.use(express.json());
+
+server.get('/recipes', (req, res) => {
+  let response = {
+    recipeNames: data.recipes.map(recipe => {
+      return recipe.name
+    })
+  };
+  res.status(200).json(response);
+});
 
 module.exports = server;
--- server.test.js
+++ server.test.js
@@ -3,3 +3,4 @@ const server = require('./server');
 it('sanity check', () => {
   expect(true).not.toBe(false);
 });
+
